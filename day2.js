45ad0b90c3b229b05a6c2834da76fcfab0476f48
Lucas Schenatto <lucas.schenatto@sap.com>
2016-12-06 22:33:19 -0200

    day 2 part 2 done

--- day2.js
+++ day2.js
@@ -1,29 +1,41 @@
 var commands = process.argv.slice(2);
 var keyboard = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-  // [undefined,undefined,    1    ,undefined,undefined],
-  // [undefined,    2    ,    3    ,    4    ,undefined],
-  // [    5    ,    6    ,    7    ,    8    ,    9    ],
-  // [undefined,   'A'   ,   'B'   ,   'C'   ,undefined],
-  // [undefined,undefined,   'D'   ,undefined,undefined]
+  [undefined,undefined,    1    ,undefined,undefined],
+  [undefined,    2    ,    3    ,    4    ,undefined],
+  [    5    ,    6    ,    7    ,    8    ,    9    ],
+  [undefined,   'A'   ,   'B'   ,   'C'   ,undefined],
+  [undefined,undefined,   'D'   ,undefined,undefined]
 ];
 var result = [];
+var coord = function(c){
+  return keyboard[c[0]][c[1]];
+}
 var up = function(current){
+  var old = current[0];
   current[0] = current[0] > 0 ? current[0] - 1 : current[0];
+  if(coord(current) === undefined)
+    current[0] = old;
   return current;
 }
 var down = function(current){
-  current[0] = current[0] < 2 ? current[0] + 1 : current[0];
+  var old = current[0];
+  current[0] = current[0] < 4 ? current[0] + 1 : current[0];
+  if(coord(current) === undefined)
+    current[0] = old;
   return current;
 }
 var left = function(current){
+  var old = current[1];
   current[1] = current[1] > 0 ? current[1] - 1 : current[1];
+  if(coord(current) === undefined)
+    current[1] = old;
   return current;
 }
 var right = function(current){
-  current[1] = current[1] < 2 ? current[1] + 1 : current[1];
+  var old = current[1];
+  current[1] = current[1] < 4 ? current[1] + 1 : current[1];
+  if(coord(current) === undefined)
+    current[1] = old;
   return current;
 }
 var doInstruction = function(instruction,current){
@@ -52,7 +64,7 @@ var findPosition = function (key){
   return [lineIndex,keyIndex];
 };
 var readLine = function readLine(line){
-  var current = result[result.length - 1] || [1,1];
+  var current = result[result.length - 1] || [2,0];
   line.split('').forEach(function(instruction){
       current = doInstruction(instruction,current.slice());
   });
@@ -62,6 +74,6 @@ commands.forEach(function(line){
   result.push(readLine(line));
 });
 var convertedResult = [];
-result.forEach(function(c){convertedResult.push(keyboard[c[0]][c[1]])} );
+result.forEach(function(c){convertedResult.push(coord(c))} );
 
 console.log(convertedResult);
