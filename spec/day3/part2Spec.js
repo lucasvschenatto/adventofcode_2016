3859ef2b6a5298b8b1eb68d668f56bd1c5cb0029
Lucas Schenatto <lucas.schenatto@sap.com>
2016-12-13 18:58:36 -0200

    completed day3 part1

---
+++ spec/day3/part2Spec.js
@@ -0,0 +1,53 @@
+describe("columnCounter", function() {
+  var counter = require('../../lib/day3/columnCounter');
+
+  // it("should count all triangles",()=>{
+  //   var count = counter([3,4,5],[9,9,9],[6,8,10]);
+  //   expect(count).toEqual(3);
+  // });
+  // it("should count only triangles",()=>{
+  //   var count = counter([3,4,5],[9,9,9],[10,10,25]);
+  //   expect(count).toEqual(2);
+  });
+  //
+  // describe("when song has been paused", function() {
+  //   beforeEach(function() {
+  //     player.play(song);
+  //     player.pause();
+  //   });
+  //
+  //   it("should indicate that the song is currently paused", function() {
+  //     expect(player.isPlaying).toBeFalsy();
+  //
+  //     // demonstrates use of 'not' with a custom matcher
+  //     expect(player).not.toBePlaying(song);
+  //   });
+  //
+  //   it("should be possible to resume", function() {
+  //     player.resume();
+  //     expect(player.isPlaying).toBeTruthy();
+  //     expect(player.currentlyPlayingSong).toEqual(song);
+  //   });
+  // });
+  //
+  // // demonstrates use of spies to intercept and test method calls
+  // it("tells the current song if the user has made it a favorite", function() {
+  //   spyOn(song, 'persistFavoriteStatus');
+  //
+  //   player.play(song);
+  //   player.makeFavorite();
+  //
+  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  // });
+  //
+  // //demonstrates use of expected exceptions
+  // describe("#resume", function() {
+  //   it("should throw an exception if song is already playing", function() {
+  //     player.play(song);
+  //
+  //     expect(function() {
+  //       player.resume();
+  //     }).toThrowError("song is already playing");
+  //   });
+  // });
+});
