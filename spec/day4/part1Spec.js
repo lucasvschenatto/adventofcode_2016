a6b8e1a9114b2e894a8667bd6b431c73debf94bb
Lucas Schenatto <lucas.schenatto@sap.com>
2016-12-14 18:32:35 -0200

    created input and initial dummy test

---
+++ spec/day4/part1Spec.js
@@ -0,0 +1,27 @@
+describe("roomValidator", function() {
+  var validator = require('../../lib/day4/roomValidator');
+  // it("should have three sides", ()=> {
+//     var triangle = makeTriangle(10,10,15);
+//     expect(triangle.sides).toEqual([10,10,15]);
+//   });
+//   it("should know when it is not a triangle", ()=>{
+//     var triangle = makeTriangle(5,10,25);
+//     expect(triangle.isTriangle()).toBeFalsy();
+//   });
+//   it("should know when it is a triangle", ()=>{
+//     var triangle = makeTriangle(10,10,10);
+//     expect(triangle.isTriangle()).toBeTruthy();
+//   });
+// });
+// describe("counter", function() {
+//   var counter = require('../../lib/day3/counter');
+//
+//   it("should count all triangles",()=>{
+//     var count = counter([3,4,5],[9,9,9],[6,8,10]);
+//     expect(count).toEqual(3);
+//   });
+//   it("should count only triangles",()=>{
+//     var count = counter([3,4,5],[9,9,9],[10,10,25]);
+//     expect(count).toEqual(2);
+//   });
+});
