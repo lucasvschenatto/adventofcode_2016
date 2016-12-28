8876dc0d2aa868082eb8622cee84be664e094978
Lucas Schenatto <lucas.schenatto@sap.com>
2016-12-28 12:44:19 -0200

    it recognizes hipernet sequence and doesn't consider it

--- spec/day7/part1Spec.js
+++ spec/day7/part1Spec.js
@@ -1,6 +1,19 @@
 describe("ABBAFinder", ()=>{
-  var ABBAFinder = require("../../lib/day7/ABBAFinder");
-  it("should find ABBA in the 'ABBA' string", ()=>{
-
+  var isAbba = require("../../lib/day7/isAbba");
+  it("should find ABBA if the IP starts with mirrored 'ab'", ()=>{
+    var result = isAbba('abba[xxxx]yyyy');
+    expect(result).toBeTruthy();
+  });
+  it("should not find ABBA if the IP has two mirrored chars", ()=>{
+    var result = isAbba('abcd[efgh]ijkl');
+    expect(result).toBeFalsy();
+  });
+  it("should find ABBA in other mirrored two chars too",()=>{
+    var result = isAbba('xyyx[efgh]ijkl');
+    expect(result).toBeTruthy();
+  });
+  it("should not find ABBA in mirrored two chars within square brackets",()=>{
+    var result = isAbba('abcd[abba]ijkl');
+    expect(result).toBeFalsy();
   });
 });
